25cb3956695f8b3e7f7e17328d8230ac3f0ccdae
ThiagoariednaB <66887727+ThiagoariednaB@users.noreply.github.com>
2022-07-29 14:07:50 -0300

    Delete Menu.js

--- Menu.js
+++
@@ -1,7 +0,0 @@
-let show = true
-const menuSection = document.querySelector('.menu1')
-const menuBurguer = menuSection.querySelector('.menuBurguer')
-menuBurguer.addEventListener('click', () => {
-  menuSection.classList.toggle('on', show)
-  show = !show
-})//Controla a ativação e desativação do MenuBurguer
